6c3431b30e482632331e190d096eec1b890adade
chidi.uba <talk2chidicfc@yahoo.com>
2020-07-15 15:40:16 +0100

    check that a valid date is parsed

--- lib/tasks/billed_orders.rake
+++ lib/tasks/billed_orders.rake
@@ -4,5 +4,10 @@ namespace :billed_orders do
     delivery_date = args[:delivery_date]
     abort 'Error: task requires a delivery date (dd-mm-yyyy) argument' if delivery_date.blank?
     abort 'Error: delivery date should be in dd-mm-yyyy format' if !delivery_date.match(/^\d{2}-\d{2}-\d{4}$/)
+
+    parsed_delivery_date = Time.zone.parse(delivery_date)
+
+  rescue ArgumentError
+    puts 'Error: please enter a valid date'
   end
 end
--- spec/tasks/billed_orders_spec.rb
+++ spec/tasks/billed_orders_spec.rb
@@ -3,16 +3,33 @@ require 'support/tasks'
 
 describe 'rake billed_orders:shipping' do
   context 'when no argument is given' do
+    let(:error_message) { 'Error: task requires a delivery date (dd-mm-yyyy) argument' }
+
     it 'raises an appropriate error' do
-      expect {task.invoke}.to raise_error('Error: task requires a delivery date (dd-mm-yyyy) argument')
+      expect {task.invoke}.to raise_error(error_message)
     end
   end
 
-  context 'when the argument is in the wrong format' do
+  context 'when an argument is given' do
     before { task.reenable }
 
-    it 'raises an appropriate error' do
-      expect {task.invoke('test')}.to raise_error('Error: delivery date should be in dd-mm-yyyy format')
+    context 'when the argument is in the wrong format' do
+      let(:argument) { ['test', '12/9/2019'].sample }
+      let(:error_message) { 'Error: delivery date should be in dd-mm-yyyy format' }
+
+      it 'raises an appropriate error' do
+        expect {task.invoke(argument)}.to raise_error(error_message)
+      end
+    end
+
+    context 'when an invalid date is given' do
+      let(:invalid_date) { '42-07-2011' }
+      let(:error_message) { "Error: please enter a valid date\n" }
+
+      it 'raises an appropriate error' do
+        expect{task.invoke(invalid_date)}.to output(error_message).to_stdout
+      end
     end
   end
+
 end
