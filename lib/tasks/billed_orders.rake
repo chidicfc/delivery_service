c2db21713d08620da98f75100b105985379ff5b2
chidi.uba <talk2chidicfc@yahoo.com>
2020-07-15 15:21:39 +0100

    add test to ensure date argument is in the correct format

--- lib/tasks/billed_orders.rake
+++ lib/tasks/billed_orders.rake
@@ -3,5 +3,6 @@ namespace :billed_orders do
   task :shipping, [:delivery_date] => :environment do |_task, args|
     delivery_date = args[:delivery_date]
     abort 'Error: task requires a delivery date (dd-mm-yyyy) argument' if delivery_date.blank?
+    abort 'Error: delivery date should be in dd-mm-yyyy format' if !delivery_date.match(/^\d{2}-\d{2}-\d{4}$/)
   end
 end
--- spec/tasks/billed_orders_spec.rb
+++ spec/tasks/billed_orders_spec.rb
@@ -4,7 +4,15 @@ require 'support/tasks'
 describe 'rake billed_orders:shipping' do
   context 'when no argument is given' do
     it 'raises an appropriate error' do
-      expect{task.execute}.to raise_error('Error: task requires a delivery date (dd-mm-yyyy) argument')
+      expect {task.invoke}.to raise_error('Error: task requires a delivery date (dd-mm-yyyy) argument')
+    end
+  end
+
+  context 'when the argument is in the wrong format' do
+    before { task.reenable }
+
+    it 'raises an appropriate error' do
+      expect {task.invoke('test')}.to raise_error('Error: delivery date should be in dd-mm-yyyy format')
     end
   end
 end
